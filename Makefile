4728135db47e922713922084da223bcb643ec084
Lucas Courot <lucas@courot.com>
2020-04-18 01:50:14 +0200

    Add run and test Makefile targets

--- Makefile
+++ Makefile
@@ -16,3 +16,11 @@ docker-pull-rebuild: ## Rebuild docker images
 
 docker-php: ## Open bash in the php image
 	docker-compose exec php bash
+
+# Gilded Rose
+
+run: ## Run Gilded Rose example app
+	docker-compose exec php php bin/app.php run
+
+test: ## Run tests with coverage
+	docker-compose exec php php vendor/bin/phpunit --testdox --coverage-text
--- bin/app.php
+++ bin/app.php
@@ -7,9 +7,9 @@ use GildedRose\GildedRose;
 use Symfony\Component\Console\Output\OutputInterface;
 
 $items = [
-    new Item('Aged Brie', 10, 10),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10),
-    new Item('Sulfuras, Hand of Ragnaros', 10, 10),
+    new Item('Aged Brie', 5, 10),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10),
+    new Item('Sulfuras, Hand of Ragnaros', 5, 10),
 ];
 
 $app = new Silly\Application();
